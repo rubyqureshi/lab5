18863bd3fd3682665fbcb219c9dd9a0541b5d0d8
rubyqureshi <jsy2794@autuni.ac.nz>
2022-04-15 00:05:49 +1200

    ping

--- user/pingpong.c
+++ user/pingpong.c
@@ -56,7 +56,7 @@ main()
         // printf("Parent Process - Writing to pipe - Message 2 is %s\n", writemessages[1]);
         // write(pipefd[1], writemessages[1], sizeof(writemessages[1]));
     }
-    wait(10);
+    wait(10*);
     if (pid == 0) {
         close(pipefd[0]);
         write(pipefd[1], writemessages[1], sizeof(writemessages[1]));
