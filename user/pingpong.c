6cf7e4c675858c5e6bd805eb5bae54b9f45dbbf9
rubyqureshi <jsy2794@autuni.ac.nz>
2022-04-14 23:21:37 +1200

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -24,15 +24,15 @@ main()
     // Child process
     if (pid == 0) {
         read(pipefds[0], readmessage, sizeof(readmessage));
-        printf("Message received by child from parent is: %s\n", readmessage);
-        //read(pipefds[0], readmessage, sizeof(readmessage));
-        // printf("Child Process - Reading from pipe - Message 2 is %s\n", readmessage);
+        printf("Child reading msg from pipe: %s\n", readmessage);
+        read(pipefds[0], readmessage, sizeof(readmessage));
+        printf("Child Process - Reading from pipe - Message 2 is %s\n", readmessage);
     } 
     else { //Parent process
-        printf("Message from Parent - %s\n", writemessages[0]);
+        printf("Message %s\n", writemessages[0]);
         write(pipefds[1], writemessages[0], sizeof(writemessages[0]));
-        // printf("Parent Process - Writing to pipe - Message 2 is %s\n", writemessages[1]);
-        // write(pipefds[1], writemessages[1], sizeof(writemessages[1]));
+        printf("Parent Process - Writing to pipe - Message 2 is %s\n", writemessages[1]);
+        write(pipefds[1], writemessages[1], sizeof(writemessages[1]));
     }
 
 
