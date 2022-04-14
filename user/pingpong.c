bf52b3245c0daf2a033d6a5d14fdacd4773dc6db
rubyqureshi <jsy2794@autuni.ac.nz>
2022-04-14 23:37:17 +1200

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,15 +5,16 @@
 void
 main()
 {
-    int pipefds[2];
+    int pipefd[2];
     int pid;
     char writemessages[2][30]={"Hi", "Hello"};
     char readmessage[30];
     
-    int returnstatus = pipe(pipefds);
+    int returnstatus = pipe(pipefd);
     
     if (returnstatus == -1){
         printf("Unable to create pipe\n");
+        exit(0);
     }
     else{
         printf("Successfully established a single pipe.\n");
@@ -23,18 +24,21 @@ main()
 
     // Child process
     if (pid == 0) {
-        read(pipefds[0], readmessage, sizeof(readmessage));
-        // printf("Child reading msg from pipe: %s\n", readmessage);
-        // read(pipefds[0], readmessage, sizeof(readmessage));
+        read(pipefd[0], readmessage, sizeof(readmessage));
+        printf("Child reading msg : %s\n", readmessage);
+        // read(pipefd[0], readmessage, sizeof(readmessage));
         // printf("Child Process - Reading from pipe - Message 2 is %s\n", readmessage);
-        printf("Child write - Message %s\n",writemessages[1]);
-        write(pipefds[0], writemessages[1], sizeof(writemessages[1]));
-    } 
-    else { //Parent process
-        printf("Message from parent : %s\n", writemessages[0]);
-        write(pipefds[1], writemessages[0], sizeof(writemessages[0]));
+        // printf("Child write - Message %s\n",writemessages[1]);
+        // write(pipefd[0], writemessages[1], sizeof(writemessages[1]));
+    }
+    //Parent process
+    else {
+        //close(pipefd[0]);
+        // printf("Message from parent : %s\n", writemessages[0]);
+        write(pipefd[1], writemessages[0], sizeof(writemessages[0]));
+        // wait(6);
         // printf("Parent Process - Writing to pipe - Message 2 is %s\n", writemessages[1]);
-        // write(pipefds[1], writemessages[1], sizeof(writemessages[1]));
+        // write(pipefd[1], writemessages[1], sizeof(writemessages[1]));
     }
 
 
